59548457fc4e49ddc1a127a7dd89bfb8e50ab23e
SonnAdolf <programeryu@163.com>
2017-05-02 19:46:12 +0800

    using WebView first time.

--- src/com/sonne/caicai/MusicPlayerActivity.java
+++ src/com/sonne/caicai/MusicPlayerActivity.java
@@ -1,9 +1,13 @@
 package com.sonne.caicai;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * @author sonne
@@ -13,12 +17,22 @@ import android.view.Window;
 public class MusicPlayerActivity extends Activity {
 	
 	public static final String TAG = "MusicPlayerActivity";
+	private WebView webview; 
 	
-    @Override
+    @SuppressLint("SetJavaScriptEnabled") @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		Log.d(TAG, "onCreate");
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.music_player_layout);
+        setContentView(R.layout.music_player_layout); 
+        webview = (WebView)findViewById(R.id.webView1);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+        	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        		view.loadUrl(url);
+        		return true;
+        	}
+        });
+        webview.loadUrl("http://118.89.29.170/RiXiang_blog/");
     }
 }
