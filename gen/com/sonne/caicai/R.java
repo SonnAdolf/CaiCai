d2cc7d5dfd0d7da77b1a742243f007f7af4fc000
SonnAdolf <programeryu@163.com>
2017-05-24 16:56:06 +0800

    imagebutton and text

--- gen/com/sonne/caicai/R.java
+++ gen/com/sonne/caicai/R.java
@@ -21,19 +21,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background_main=0x7f020000;
+        public static final int guitar=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int love=0x7f020003;
+        public static final int moviebtn_img=0x7f020004;
+        public static final int teemo=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int button2=0x7f080003;
-        public static final int button3=0x7f080005;
-        public static final int movie_btn=0x7f080004;
-        public static final int music_btn=0x7f080002;
-        public static final int music_list_view=0x7f080007;
-        public static final int teemo=0x7f080001;
-        public static final int textView1=0x7f080000;
-        public static final int videoView=0x7f080006;
-        public static final int webView1=0x7f080008;
+        public static final int action_settings=0x7f08000a;
+        public static final int main_words=0x7f080004;
+        public static final int movie_btn=0x7f080003;
+        public static final int movie_txt=0x7f080005;
+        public static final int music_list_view=0x7f080008;
+        public static final int radio_btn=0x7f080001;
+        public static final int radio_txt=0x7f080002;
+        public static final int teemo=0x7f080000;
+        public static final int teemo_txt=0x7f080006;
+        public static final int videoView=0x7f080007;
+        public static final int webView1=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/sonne/caicai/MainActivity.java
+++ src/com/sonne/caicai/MainActivity.java
@@ -2,13 +2,15 @@ package com.sonne.caicai;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * @author sonne
@@ -29,7 +31,21 @@ public class MainActivity extends Activity {
 			String tempData = savedInstanceState.getString("data_key");
 			Log.d(TAG, tempData);
 		}
-		Button musicLstBtn = (Button) findViewById(R.id.music_btn);
+		
+		TextView tv1 = (TextView)findViewById(R.id.main_words); 
+		tv1.getPaint().setFakeBoldText(true);
+		Typeface mtypeface=Typeface.createFromAsset(getAssets(),"huakangshaonv.ttf");
+		tv1.setTypeface(mtypeface);
+		
+		TextView tv2 = (TextView)findViewById(R.id.movie_txt);
+		TextView tv3 = (TextView)findViewById(R.id.radio_txt);
+		TextView tv4 = (TextView)findViewById(R.id.teemo_txt);
+		tv2.setTypeface(mtypeface);
+		tv3.setTypeface(mtypeface);
+		tv4.setTypeface(mtypeface);
+		
+		
+		ImageButton musicLstBtn = (ImageButton) findViewById(R.id.radio_btn);
 		musicLstBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -38,7 +54,7 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		Button movieBtn = (Button) findViewById(R.id.movie_btn);
+		ImageButton movieBtn = (ImageButton) findViewById(R.id.movie_btn);
 		movieBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -47,7 +63,7 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		Button teemoBtn = (Button) findViewById(R.id.teemo);
+		ImageButton teemoBtn = (ImageButton) findViewById(R.id.teemo);
 		teemoBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
